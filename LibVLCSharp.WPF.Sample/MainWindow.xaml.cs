0134940b37188df86e5b7191df409c8b11602fb3
Martin Finkel <martin@videolabs.io>
2017-12-12 05:10:26 +0100

    Minimal WPF experiment #19

---
+++ LibVLCSharp.WPF.Sample/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Interop;
+using VideoLAN.LibVLC;
+
+namespace LibVLCSharp.WPF.Sample
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                var window = GetWindow(this);
+                var wi = new WindowInteropHelper(window);
+
+                var instance = new Instance();
+                var mp = new MediaPlayer(instance)
+                {
+                    Hwnd = wi.Handle,
+                    Media = new Media(instance, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation)
+                };
+                mp.Play();
+            };
+        }
+    }
+}
